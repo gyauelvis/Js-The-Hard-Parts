d2633cf672eb6e488c6ee7174fed7321e460bad2
g.elvis <gyauelvis@gmail.com>
2023-06-11 20:48:16 +0100

    exercise

--- closure_exercises.js
+++ closure_exercises.js
@@ -34,7 +34,6 @@ const onceFunc = once(addByTwo);
 console.log(onceFunc(4)); // => should log 6
 console.log(onceFunc(10)); // => should log 6
 console.log(onceFunc(9001)); // => should log 6
-
 // Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.
 
 let after = (number, callback) => {
